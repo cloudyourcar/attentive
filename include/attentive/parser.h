c50d0b58058890088263e71622c918a2007ca6c6
Kosma Moczek <kosma@cloudyourcar.com>
2014-06-25 17:45:30 +0200

    add missing copyright headers

--- include/attentive/parser.h
+++ include/attentive/parser.h
@@ -1,3 +1,11 @@
+/*
+ * Copyright © 2014 Kosma Moczek <kosma@cloudyourcar.com>
+ * This program is free software. It comes without any warranty, to the extent
+ * permitted by applicable law. You can redistribute it and/or modify it under
+ * the terms of the Do What The Fuck You Want To Public License, Version 2, as
+ * published by Sam Hocevar. See the COPYING file for more details.
+ */
+
 #ifndef ATTENTIVE_PARSER_H
 #define ATTENTIVE_PARSER_H
 
