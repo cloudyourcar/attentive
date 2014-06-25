edccdcc713d4791881e2826e4d2410e587506370
Kosma Moczek <kosma@cloudyourcar.com>
2014-06-25 18:49:25 +0200

    sim800: add basic callbacks

--- src/modem/sim800.c
+++ src/modem/sim800.c
@@ -13,11 +13,46 @@
 
 #include "generic.h"
 
+static const char *sim800_urc_responses[] = {
+    "+CIPRXGET: 1,",    /* incoming socket data notification */
+    "+FTPGET: 1,",      /* FTP state change notification */
+    "+PDP: DEACT",      /* PDP disconnected */
+    "+SAPBR 1: DEACT",  /* PDP disconnected (for SAPBR apps) */
+    NULL
+};
 
 struct cellular_sim800 {
     struct cellular dev;
+
+    int ftpget1_status;
 };
 
+static enum at_response_type scan_line(const void *line, size_t len, void *arg)
+{
+    (void) len; /* FIXME: we could do better */
+    (void) arg;
+
+    if (at_prefix_in_table(line, sim800_urc_responses))
+        return AT_RESPONSE_URC;
+
+    return AT_RESPONSE_UNKNOWN;
+}
+
+static void handle_urc(const void *line, size_t len, void *arg)
+{
+    (void) len;
+    struct cellular_sim800 *modem = arg;
+
+    printf("[SIM800@%p] URC: %.*s\n", arg, (int) len, line);
+
+    if (sscanf(line, "+FTPGET: 1,%d", &modem->ftpget1_status) == 1)
+        return;
+}
+
+static const struct at_callbacks sim800_callbacks = {
+    .scan_line = scan_line,
+    .handle_urc = handle_urc,
+};
 
 static const struct cellular_device_ops sim800_device_ops = {
     .imei = generic_op_imei,
@@ -34,7 +69,22 @@ static const struct cellular_clock_ops sim800_clock_ops = {
     .settime = generic_op_settime,
 };
 
+static int sim800_attach(struct cellular *modem)
+{
+    at_set_callbacks(modem->at, &sim800_callbacks, (void *) modem);
+    return 0;
+}
+
+static int sim800_detach(struct cellular *modem)
+{
+    at_set_callbacks(modem->at, NULL, NULL);
+    return 0;
+}
+
 static const struct cellular_ops sim800_ops = {
+    .attach = sim800_attach,
+    .detach = sim800_detach,
+
     .device = &sim800_device_ops,
     .network = &sim800_network_ops,
     .clock = &sim800_clock_ops,
